fd6d48393badf4f4e7dcae0a9d6e748fda76cf64
Masahiro Hiramori <mhg00g13@gmail.com>
2022-07-20 09:41:12 +0900

    add experimental code

--- main.ts
+++ main.ts
@@ -1,137 +1,68 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownRenderChild, MarkdownPostProcessorContext } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export class Emoji extends MarkdownRenderChild {
+	static ALL_EMOJIS: Record<string, string> = {
+		":+1:": "👍",
+		":sunglasses:": "😎",
+		":smile:": "😄",
+	};
 
-interface MyPluginSettings {
-	mySetting: string;
+	text: string;
+
+	constructor(containerEl: HTMLElement, text: string) {
+		super(containerEl);
+
+		this.text = text;
+	}
+
+	onload() {
+		const emojiEl = this.containerEl.createSpan({
+			text: Emoji.ALL_EMOJIS[this.text] ?? this.text,
+		});
+		this.containerEl.replaceWith(emojiEl);
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function createCheckboxElement(checked: boolean): HTMLElement {
+	const element = document.createElement("input");
+	element.type = "checkbox";
+	element.checked = checked;
+	return element;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function EnhanceLogseqTaskStatusInPreviewMode(
+	element : HTMLElement,
+	context: MarkdownPostProcessorContext
+) {
+	const entries = element.querySelectorAll("li[data-line]");
 
+	entries.forEach((entry) => {
+		if (entry.innerHTML.startsWith("TODO ")) {
+			entry.insertAdjacentElement(
+				"afterbegin",
+				createCheckboxElement(false)
+			)
+		} else if (entry.innerHTML.startsWith("DOING ")) {
+			entry.insertAdjacentElement(
+				"afterbegin",
+				createCheckboxElement(false)
+			)
+		} else if (entry.innerHTML.startsWith("DONE ")) {
+			entry.insertAdjacentElement(
+				"afterbegin",
+				createCheckboxElement(true)
+			)
+		}
+	})
+}
+
+export default class LogseqPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log("Loading Loseq Compatibility Plugin...");
+		this.registerMarkdownPostProcessor(EnhanceLogseqTaskStatusInPreviewMode);
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	async onunload() {
+		console.log("Unloading Loseq Compatibility Plugin...");
 	}
 }
